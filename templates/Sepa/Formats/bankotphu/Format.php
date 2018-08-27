b63631ac4aac732f449ce4e45898ca3ecdc5e53c
Patrick Figel <pfigel@greenpeace.org>
2018-08-27 13:26:35 +0200

    Set display_name with original encoding
    CiviSEPA converts display_name to ASCII, but we need the original
    name.

--- templates/Sepa/Formats/bankotphu/Format.php
+++ templates/Sepa/Formats/bankotphu/Format.php
@@ -46,8 +46,13 @@ class CRM_Sepa_Logic_Format_bankotphu extends CRM_Sepa_Logic_Format {
   public function extendTransaction(&$txn, $creditor_id) {
 	  $contribution_recur_id = $txn["contribution_recur_id"];
     $result = CRM_Core_DAO::executeQuery("select m.entity_id as membership_id from civicrm_value_membership_payment m where m.membership_recurring_contribution = " . $contribution_recur_id);
-    while ($result->fetch()){
+    while ($result->fetch()) {
       $txn['membership_id'] = $result->membership_id;
+    }
+    // set display_name in original encoding
+    $result = CRM_Core_DAO::executeQuery("select display_name from civicrm_contact where id = %0", [[$txn['contact_id'], 'Integer']]);
+    while ($result->fetch()) {
+      $txn['display_name'] = $result->display_name;
     }
 	  return $txn;
   }
