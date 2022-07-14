74c60fead12760c8c0948dc8c2ef45cde9949198
Patrick Figel <pfigel@greenpeace.org>
2022-07-14 17:10:29 +0200

    GP-22919 Upgrade civix

--- sepacustom.php
+++ sepacustom.php
@@ -199,7 +199,6 @@ function sepacustom_civicrm_modify_txgroup_reference(&$reference, $creditor_id, 
   }
 }
 
-
 /**
  * Connect newly generated SEPA contributions
  *  to the membership/contract
@@ -234,7 +233,6 @@ function sepacustom_civicrm_modify_txmessage(&$txmessage, $info, $creditor) {
   $txmessage = "Greenpeace Beitrag Danke - {$info['reference']}";
 }
 
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -242,17 +240,6 @@ function sepacustom_civicrm_modify_txmessage(&$txmessage, $info, $creditor) {
  */
 function sepacustom_civicrm_config(&$config) {
   _sepacustom_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function sepacustom_civicrm_xmlMenu(&$files) {
-  _sepacustom_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -311,51 +298,19 @@ function sepacustom_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
+ * Implements hook_civicrm_postInstall().
  *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
  */
-function sepacustom_civicrm_managed(&$entities) {
-  _sepacustom_civix_civicrm_managed($entities);
+function sepacustom_civicrm_postInstall() {
+  _sepacustom_civix_civicrm_postInstall();
 }
 
 /**
- * Implements hook_civicrm_caseTypes().
+ * Implements hook_civicrm_entityTypes().
  *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
  */
-function sepacustom_civicrm_caseTypes(&$caseTypes) {
-  _sepacustom_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function sepacustom_civicrm_angularModules(&$angularModules) {
-_sepacustom_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function sepacustom_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _sepacustom_civix_civicrm_alterSettingsFolders($metaDataFolders);
+function sepacustom_civicrm_entityTypes(&$entityTypes) {
+  _sepacustom_civix_civicrm_entityTypes($entityTypes);
 }
