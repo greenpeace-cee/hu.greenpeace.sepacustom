cc2c9119be83e51d97b43b0574f539ecdfdba063
Patrick Figel <pfigel@greenpeace.org>
2024-09-26 11:30:12 +0200

    GP-43955 Upgrade civix

--- sepacustom.php
+++ sepacustom.php
@@ -252,15 +252,6 @@ function sepacustom_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function sepacustom_civicrm_uninstall() {
-  _sepacustom_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
@@ -270,47 +261,4 @@ function sepacustom_civicrm_enable() {
 
   $customData = new CRM_Sepacustom_CustomData('hu.greenpeace.sepacustom');
   $customData->syncOptionGroup(__DIR__ . '/resources/formats_option_group.json');
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function sepacustom_civicrm_disable() {
-  _sepacustom_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function sepacustom_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _sepacustom_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function sepacustom_civicrm_postInstall() {
-  _sepacustom_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function sepacustom_civicrm_entityTypes(&$entityTypes) {
-  _sepacustom_civix_civicrm_entityTypes($entityTypes);
 }
